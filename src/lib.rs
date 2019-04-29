a4037d6912d415b742b15f6a4ee34eb8d0488228
elana <ElanaL1@gmail.com>
2019-04-29 12:01:02 -0400

    - fix errors in line parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,13 @@
 use std::fs;
 use std::error::Error;
 
+mod parser;
+
 pub fn run(file: File) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(file.filename)?;
+    let contents = fs::read_to_string(&file.filename)?;
+
+    parse_lines(contents);
+    write_file(&file.filename);
 
     Ok(())
 }
@@ -22,4 +27,23 @@ impl File {
 
         Ok(File { filename })
     }
+}
+
+pub fn parse_lines(content: String) {
+    let non_empty_lines: Vec<&str> = content.lines().
+        filter(|line| !line.is_empty())
+        .collect();
+
+    for line in non_empty_lines {
+        parse_line(line);
+    }
+}
+
+pub fn parse_line(line: &str) {
+    parser::Parser::new(line);
+}
+
+//TODO: implement writing to a file
+pub fn write_file(filename: &str) {
+
 }
