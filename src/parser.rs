174637d6e47ffe4c9f84596196ff5550391c858a
elana <ElanaL1@gmail.com>
2019-04-29 14:43:12 -0400

    - parse lines into command-types

--- src/parser.rs
+++ src/parser.rs
@@ -22,15 +22,67 @@ pub fn parse_lines(content: &str) {
 pub fn parse_line(line: &str) {
     // remove comments
 //    let commented_segment = line.find("//").unwrap_or(line.len());
+    //TODO: make this cleaner
     let split_line: Vec<&str> = line.splitn(2,"//").collect();
     let clean_line = split_line[0];
     //identify type of command
-//    get_command(clean_line);
+    get_command(clean_line);
     //create command and add to vector that will be passed to CodeWriter later
 }
 
-enum CommandType {
-    Arithmetic {operation: String},
-    Push{var_type: String, var: u32},
-    Pop{var_type: String, var: u32},
+//TODO: make this cleaner
+//TODO: check for input errors?
+fn get_command(line: &str) -> CommandType {
+    if line.contains("push") {
+        return parse_push(line)
+    }
+    else if line.contains("pop") {
+        return parse_pop(line)
+    }
+//TODO: check for arithmetic operation explicitly later
+    let operation = line.replace("\\s","");
+    CommandType::Arithmetic{operation}
 }
+
+fn parse_operations (line: &str) -> (Option<&str, >, Option<&str, >){
+//    let mut iter = line.split_ascii_whitespace();
+    let mut iter = line.split_whitespace();
+    iter.next();
+    let var_type = iter.next();
+    let var = iter.next();
+    (var_type, var)
+}
+
+//TODO: refactor parse_pop and parse_push into one method
+fn parse_pop (line: &str) -> CommandType {
+    if let (Some(var_type), Some(var)) = parse_operations(line){
+        return CommandType::Pop{var_type, var}
+    }
+    panic!("could not parse line")
+}
+
+fn parse_push (line: &str) -> CommandType {
+    if let (Some(var_type), Some(var)) = parse_operations(line) {
+        return CommandType::Push{var_type, var}
+    }
+    panic!("could not parse line")
+}
+
+enum CommandType<'a> {
+    Arithmetic {operation: String},
+    Push{var_type: &'a str, var: &'a str},
+    Pop{var_type: &'a str, var: &'a str},
+}
+
+/*
+enum Arithmetic_Commands {
+    add,
+    sub,
+    neg,
+    eq,
+    gt,
+    lt,
+    and,
+    or,
+    not
+}*/
