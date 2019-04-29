327180d63bfa0f65cffb8f367c72a6eb417dd965
elana <ElanaL1@gmail.com>
2019-04-29 15:16:58 -0400

    - parse lines into command-types

--- src/parser.rs
+++ src/parser.rs
@@ -20,32 +20,33 @@ pub fn parse_lines(content: &str) {
 }
 
 pub fn parse_line(line: &str) {
-    // remove comments
+//    remove comments:
 //    let commented_segment = line.find("//").unwrap_or(line.len());
-    //TODO: make this cleaner
-    let split_line: Vec<&str> = line.splitn(2,"//").collect();
+//    TODO: make this cleaner
+    let split_line: Vec<&str> = line.splitn(2, "//").collect();
     let clean_line = split_line[0];
-    //identify type of command
-    get_command(clean_line);
-    //create command and add to vector that will be passed to CodeWriter later
+//    identify type of command
+    if !clean_line.is_empty() {
+        let command = get_command(&clean_line);
+        print!("{:#?}", command);
+    }
+//    create command and add to vector that will be passed to CodeWriter later
 }
 
 //TODO: make this cleaner
 //TODO: check for input errors?
 fn get_command(line: &str) -> CommandType {
     if line.contains("push") {
-        return parse_push(line)
-    }
-    else if line.contains("pop") {
-        return parse_pop(line)
+        return parse_push(line);
+    } else if line.contains("pop") {
+        return parse_pop(line);
     }
 //TODO: check for arithmetic operation explicitly later
-    let operation = line.replace("\\s","");
-    CommandType::Arithmetic{operation}
+    let operation = line.replace("\\s", "");
+    CommandType::Arithmetic { operation }
 }
 
-fn parse_operations (line: &str) -> (Option<&str, >, Option<&str, >){
-//    let mut iter = line.split_ascii_whitespace();
+fn parse_operations(line: &str) -> (Option<&str, >, Option<&str, >) {
     let mut iter = line.split_whitespace();
     iter.next();
     let var_type = iter.next();
@@ -54,35 +55,23 @@ fn parse_operations (line: &str) -> (Option<&str, >, Option<&str, >){
 }
 
 //TODO: refactor parse_pop and parse_push into one method
-fn parse_pop (line: &str) -> CommandType {
-    if let (Some(var_type), Some(var)) = parse_operations(line){
-        return CommandType::Pop{var_type, var}
-    }
-    panic!("could not parse line")
-}
-
-fn parse_push (line: &str) -> CommandType {
+fn parse_pop(line: &str) -> CommandType {
     if let (Some(var_type), Some(var)) = parse_operations(line) {
-        return CommandType::Push{var_type, var}
+        return CommandType::Pop { var_type, var };
     }
     panic!("could not parse line")
 }
 
-enum CommandType<'a> {
-    Arithmetic {operation: String},
-    Push{var_type: &'a str, var: &'a str},
-    Pop{var_type: &'a str, var: &'a str},
+fn parse_push(line: &str) -> CommandType {
+    if let (Some(var_type), Some(var)) = parse_operations(line) {
+        return CommandType::Push { var_type, var };
+    }
+    panic!("could not parse line")
 }
 
-/*
-enum Arithmetic_Commands {
-    add,
-    sub,
-    neg,
-    eq,
-    gt,
-    lt,
-    and,
-    or,
-    not
-}*/
+#[derive(Debug)]
+enum CommandType<'a> {
+    Arithmetic { operation: String },
+    Push { var_type: &'a str, var: &'a str },
+    Pop { var_type: &'a str, var: &'a str },
+}
